b1e07ba817de5e4d6a9065fd12af38d97635a298
Bryan <bryan@threeheadz.com>
2022-11-22 16:20:28 -0600

    Bam.Net.Core is now Bam.Core 2.0 beta (#1)

---
+++ bam.core/_core/_ServiceProxy/_Secure/ApiKeyResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Bam.Net.ServiceProxy.Encryption // core
+{
+    public partial class ApiHmacKeyResolver
+    {
+    }
+}
